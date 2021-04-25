44ff68e5b2c1a23b1f3df4b3613784fdefb742cb
chayada08 <chayada.0802@mail.kmutt.ac.th>
2021-04-25 07:47:07 +0700

    ข้อ1การบ้าน4

---
+++ PASCAL’S TRIANGLE Ver Fin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PASCAL_S_TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            while (rows < 0)
+            {
+                if (rows < 0)
+                { Console.WriteLine("Invalid Pascal’s triangle row number."); }
+                
+                int row2 = int.Parse(Console.ReadLine());
+                rows = row2;
+            }
+
+            cal(rows);
+
+
+
+        }
+        static void cal(int rows)
+        {
+            int val = 0, i, j;
+            for (i = 0; i <= rows; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        val = 1;
+                    else
+                        val = val * (i - j + 1) / j;
+                    Console.Write(val + " ");
+                }
+                Console.WriteLine();
+                
+            }
+            
+                }
+            }
+    }
+
